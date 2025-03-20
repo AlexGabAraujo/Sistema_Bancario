d129b065fa0cd883b2525911268a2154cc6f6828
Alex Araujo <alexgabriel.araujo@hotmail.com>
2025-03-20 09:42:09 -0300

    basic

---
+++ Sistema_Bancario/ContaCorrente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Bancario
+{
+    public class ContaCorrente : ContaBancaria
+    {
+        public ContaCorrente(int numeroConta, string titular, double saldo) : base(numeroConta, titular, saldo)
+        {
+            TaxaSaque = 5;
+            Tipo = 1;
+        }
+
+        
+    }
+}
---
+++ Sistema_Bancario/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Sistema_Bancario
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1C = new ContaCorrente(1, "Alex", 0.00);
+            conta1C.ExibirSaldo();
+            conta1C.Depositar(200);
+            conta1C.ExibirSaldo();
+            conta1C.Sacar(200);
+            conta1C.ExibirSaldo();
+
+            ContaPoupanca conta2P = new ContaPoupanca(1, "Joao", 1500.00);
+            conta2P.ExibirSaldo();
+            conta2P.Depositar(200);
+            conta2P.ExibirSaldo();
+            conta2P.Sacar(200);
+            conta2P.ExibirSaldo();
+        }
+    }
+}
